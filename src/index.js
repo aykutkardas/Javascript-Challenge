eb74a265c35fac9c1804f5d7fb4dbaac8c42b719
aykutkardas <aykutkrds@gmail.com>
2018-04-08 21:46:37 +0300

    initial

--- src/index.js
+++ src/index.js
@@ -2,72 +2,72 @@
 
 class Mylodash {
 
-    isObject(obj: any): boolean {
-        return typeof obj === "object";
-    }
+	isObject(obj: any): boolean {
+		return typeof obj === "object";
+	}
 
-    isArray(arr: any): boolean {
-        return Array.isArray(arr);
-    }
+	isArray(arr: any): boolean {
+		return Array.isArray(arr);
+	}
 
-    isFunction(fn: any): boolean {
-        return typeof fn === "function";
-    }
+	isFunction(fn: any): boolean {
+		return typeof fn === "function";
+	}
 
 
-    keys(obj: any): boolean | Array<string> {
-        if(!this.isObject(obj))
-            return false;
+	keys(obj: any): boolean | Array<string> {
+		if(!this.isObject(obj))
+			return false;
 
-        let keys: Array<string> = [];
-        let i: string;
+		let keys: Array<string> = [];
+		let i: string;
 
-        for(i in obj)
-            keys.push(i);
+		for(i in obj)
+			keys.push(i);
 
-        return keys;
-    }
+		return keys;
+	}
 
-    values(obj: any): boolean | Array<mixed> {
-        if (!this.isObject(obj))
+	values(obj: any): boolean | Array<mixed> {
+		if (!this.isObject(obj))
 			return false;
 
 		let values: Array<mixed> = [];
-        let i: string;
+		let i: string;
 
 		for (i in obj)
 			values.push(obj[i]);
 
 		return values;
-    }
+	}
 
-    entries(obj: any): boolean | Array<[string, any]> {
+	entries(obj: any): boolean | Array<[string, any]> {
 		if (!this.isObject(obj))
 			return false;
 
 		let entries: Array<[string, any]> = [];
-        let i: string;
+		let i: string;
 
 		for (i in obj)
 			entries.push([i, obj[i]]);
 
 		return entries;
-    }
+	}
 
-    clone(obj: any): boolean | Object {
+	clone(obj: any): boolean | Object {
 		if (!this.isObject(obj))
 			return false;
 
 		return Object.assign({}, obj);
-    }
+	}
 
 
-    filter(arr: any, cb: any): boolean | Array<mixed> {
-        if ((!arr && !cb) && !this.isArray(arr) || !this.isFunction(cb))
+	filter(arr: any, cb: any): boolean | Array<mixed> {
+		if ((!arr && !cb) && !this.isArray(arr) || !this.isFunction(cb))
 			return false;
 
-        let filtered: Array<mixed> = [];
-        let i: number = 0;
+		let filtered: Array<mixed> = [];
+		let i: number = 0;
 
 		for (; i < arr.length; i++) {
 			if (cb(arr[i]))
@@ -75,14 +75,15 @@ class Mylodash {
 		}
 
 		return filtered;
-    }
+	}
 
-    map(arr: any, cb: any): boolean | Array<mixed> {
+	map(arr: any, cb: any): boolean | Array<mixed> {
 		if ((!arr && !cb) && !this.isArray(arr) || !this.isFunction(cb))
 			return false;
 
 		let temp: Array<mixed> = [];
-        let i: number = 0;
+		let i: number = 0;
+
 		for (; i < arr.length; i++) {
 			temp.push(arr[i]);
 		}
@@ -90,7 +91,7 @@ class Mylodash {
 		return temp;
 	}
 
-    find(arr: any, cb: any): any {
+	find(arr: any, cb: any): any {
 		if ((!arr && !cb) && !this.isArray(arr) || !this.isFunction(cb))
 			return false;
 
@@ -100,33 +101,35 @@ class Mylodash {
 		}
 	}
 
-    includes(arr: any, item: any): any {
+	includes(arr: any, item: any): any {
 		if (!this.isArray(arr) && item)
 			return false;
 
-        let i: number = 0;
+		let i: number = 0;
+
 		for (; i < arr.length; i++) {
 			if (arr.indexOf(item) > -1)
 				return item;
 		}
 	}
 
-    each(arr: any, cb: any): boolean | void {
+	each(arr: any, cb: any): boolean | void {
 		if ((!arr && !cb) && !this.isArray(arr) || !this.isFunction(cb))
 			return false;
 
-        let i: number = 0;
+		let i: number = 0;
+
 		for (; i < arr.length; i++) {
 			cb(arr[i]);
 		}
-
 	}
 
-    every(arr: any, cb: any): boolean {
+	every(arr: any, cb: any): boolean {
 		if ((!arr && !cb) && !this.isArray(arr) || !this.isFunction(cb))
 			return false;
 
-        let i: number = 0;
+		let i: number = 0;
+
 		for (; i < arr.length; i++) {
 			if(!cb(arr[i]))
 				return false;
@@ -135,11 +138,12 @@ class Mylodash {
 		return true;
 	}
 
-    some(arr: any, cb: any): boolean {
+	some(arr: any, cb: any): boolean {
 		if ((!arr && !cb) && !this.isArray(arr) || !this.isFunction(cb))
 			return false;
 
-        let i: number = 0;
+		let i: number = 0;
+
 		for (; i < arr.length; i++) {
 			if (cb(arr[i]))
 				return true;
@@ -148,12 +152,12 @@ class Mylodash {
 		return false;
 	}
 
-    reduce(arr: any, cb: any): any {
+	reduce(arr: any, cb: any): any {
 		if ((!arr && !cb) && !this.isArray(arr) || !this.isFunction(cb))
 			return false;
-		
+
 		let result: any = arr[0];
-        let i: number = 1;
+		let i: number = 1;
 
 		for(; i < arr.length; i++) {
 			result = cb(result, arr[i]);
